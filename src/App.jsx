0ed59c0115a85838a65a7abdd0c6f7b0f5dc4305
Cristhian B <cristhianblaffita@gmail.com>
2026-02-28 08:27:46 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import removeIcon from "@/assets/icons/ui/close.png"
+
+const App = () => {
+  
+  return (
+    <>
+      <p>S-Mall</p>
+    </>  
+  )
+}
+
+export default App
