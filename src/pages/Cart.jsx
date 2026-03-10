1625ce9751a88b0ae1f9c23e550b1847d83cb4c3
Cristhian B <cristhianblaffita@gmail.com>
2026-03-09 21:44:50 -0400

    feat: add empty cart feature

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,11 @@
+import { useCart } from "@/contexts/cart/useCart"
+
 const Cart = () => {
+  const { items } = useCart()
   
   return (
     <main
-      className="w-full8 relative flex flex-col gap-8"
+      className="w-full h-full relative flex flex-col gap-8"
     >
       <div
         className="w-full bg-surface p-32 shadow-sm mt-8"
@@ -11,10 +14,12 @@ const Cart = () => {
       </div>
       
       <section
-        className="w-full bg-surface shadow-sm mb-120"
+        className="w-full h-full bg-surface shadow-sm mb-120 text-gray text-center"
       >
+        {items ? false : <p className="p-32">Your S-Mall cart is empty</p>}
       </section>
-      <div
+      
+      { items ? (<div
         className="fixed bottom-left flex flex-col w-full bg-surface p-32 shadow-lg mt-8
         gap-8"
       >
@@ -25,8 +30,8 @@ const Cart = () => {
         </div>
         <button
           className="primary-button w-full p-16 rounded-md text-normal text-lg"
-        >Checkout</button>
-      </div>
+        >Checkout</button> 
+      </div>) : false}
     </main>
   )
 }
