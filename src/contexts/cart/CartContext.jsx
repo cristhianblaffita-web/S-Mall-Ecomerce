ef5e46b7f2f8ff43d2b230e92e1cfdfa326ef64c
cristhianblaffita-web <cristhianblaffita@gmail.com>
2026-03-19 17:10:30 -0400

    fix: fix image max-width in product details

--- src/contexts/cart/CartContext.jsx
+++ src/contexts/cart/CartContext.jsx
@@ -6,42 +6,43 @@ export const CartContext = createContext()
 export const CartProvider = ({ children }) => {
 
   const [cartItems, setCartItems] = useState([])
-  
-  const [cartAnimation, setCartAnimation] = useState("")
-  
-  const cartItemsQtty = cartItems.reduce((total, item) => {
-  return total + item.qtty
-}, 0)
 
-   
+  const [cartAnimation, setCartAnimation] = useState("")
+
+  const cartItemsQtty = cartItems.reduce((total, item) => {
+    return total + item.qtty
+  }, 0)
+
+
   useEffect(() => {
 
-  if (cartItemsQtty > 0) {
-    setCartAnimation("cart-bump")
+    if (cartItemsQtty > 0) {
+      setCartAnimation("cart-bump")
 
-    const timer = setTimeout(() => {
-      setCartAnimation("cart-shake")
-    }, 100)
+      const timer = setTimeout(() => {
+        setCartAnimation("cart-shake")
+      }, 300)
 
-    return () => clearTimeout(timer)
-  }
+      return () => clearTimeout(timer)
+    }
 
-  setCartAnimation("")
+    setCartAnimation("")
 
-}, [cartItemsQtty])
+  }, [cartItemsQtty])
 
   const addToCart = (product) => {
     const cartItem = createCartItem(product)
-    
+
     setCartItems(prev => {
       const productExists = prev.find(item => item.id === cartItem.id)
-      
+
       if (productExists) {
-        return prev.map(item => item.id === cartItem.id ? {...item, qtty:
-        item.qtty + 1, subtotal: (item.qtty + 1) * item.price
-        } : item) 
+        return prev.map(item => item.id === cartItem.id ? {
+          ...item, qtty:
+            item.qtty + 1, subtotal: (item.qtty + 1) * item.price
+        } : item)
       }
-      
+
       return [...prev, cartItem]
     })
 
@@ -50,29 +51,32 @@ export const CartProvider = ({ children }) => {
   const removeFromCart = (id) => {
     setCartItems(prev => prev.filter(item => item.id !== id))
   }
-  
+
   const decreaseItemQtty = (id) => {
     setCartItems(prev => {
-     return prev.map(item => item.id === id ? {...item, qtty: item.qtty - 1,
-     subtotal: (item.qtty - 1) * item.price} :
-     item).filter(item => item.qtty > 0)
+      return prev.map(item => item.id === id ? {
+        ...item, qtty: item.qtty - 1,
+        subtotal: (item.qtty - 1) * item.price
+      } :
+        item).filter(item => item.qtty > 0)
     })
   }
-  
+
   const increaseItemQtty = (id) => {
     setCartItems(prev => {
-     return prev.map(item => item.id === id ? {...item, qtty: item.qtty + 1,
-     subtotal: (item.qtty + 1) * item.price
-     } :
-     item)
+      return prev.map(item => item.id === id ? {
+        ...item, qtty: item.qtty + 1,
+        subtotal: (item.qtty + 1) * item.price
+      } :
+        item)
     })
   }
-  
+
   const calculateCartTotal = () => {
     return cartItems.reduce((total, item) => {
-    return total + item.price * item.qtty
-  }, 0)
-}
+      return total + item.price * item.qtty
+    }, 0)
+  }
 
   return (
     <CartContext.Provider
--- src/features/details/components/ProductDetails.jsx
+++ src/features/details/components/ProductDetails.jsx
@@ -1,167 +1,167 @@
 import "./ProductDetails.css"
 import { useCart } from "@/contexts/cart/useCart"
 import ratingIcon from "@/assets/icons/ui/star.png"
-import arrowDown  from "@/assets/icons/ui/arrow-down.png"
+import arrowDown from "@/assets/icons/ui/arrow-down.png"
 import addToCartIcon from "@/assets/icons/ui/add-to-cart.png"
 
 const ProductDetails = (
   {
-  productId = null,
-  productThumbnail = null,
-  productImages = [],
-  productTitle = null,
-  productPrice = 0,
-  productDiscount = 0,
-  productRating = 0,
-  productDescription = null,
-  productStock = 0,
-  productBrand = "---",
-  productWeight = 0,
-  productDimensions = [],
-  productWarranty = "No warranty"
-}) => {
-  
+    productId = null,
+    productThumbnail = null,
+    productImages = [],
+    productTitle = null,
+    productPrice = 0,
+    productDiscount = 0,
+    productRating = 0,
+    productDescription = null,
+    productStock = 0,
+    productBrand = "---",
+    productWeight = 0,
+    productDimensions = [],
+    productWarranty = "No warranty"
+  }) => {
+
   const { addToCart } = useCart()
-  
+
   const product = {
     id: productId,
     title: productTitle,
     image: productThumbnail,
     price: productPrice
   }
-  
+
   return (
     <section
-  className="w-full flex flex-col bg-surface"
->
-
-  <ul
-    className="w-full flex overflow-x-auto gap-16 p-16"
-    style={{
-      listStyle: "none"
-    }}
-  >
-    {productImages.map((img, index) => (
-      <li
-        key={index}
-        
-      >
-        <img
-          className="details-image bg-background w-full rounded-md p-8"
-          src={img}
-          onLoad={(e) => e.currentTarget.classList.add("loaded")}
-        />
-      </li>
-    ))}
-  </ul>
-
-  <div
-    className="bg-surface flex flex-col gap-16 p-24"
-  >
-    
-    <h1 className="text-left">
-      {productTitle}
-    </h1>
-
-    <div
-      className="flex justify-between items-center"
+      className="w-full flex flex-col bg-surface"
     >
-      <h2 className="product-price">
-        ${productPrice.toFixed(2)}
-      </h2>
 
-      <span
-        className="flex items-center gap-8"
+      <ul
+        className="w-full flex overflow-x-auto gap-16 p-16"
+        style={{
+          listStyle: "none"
+        }}
       >
+        {productImages.map((img, index) => (
+          <li
+            key={index}
+
+          >
+            <img
+              className="details-image bg-background w-full rounded-md p-8"
+              src={img}
+              onLoad={(e) => e.currentTarget.classList.add("loaded")}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <div
+        className="bg-surface flex flex-col gap-16 p-24"
+      >
+
+        <h1 className="text-left">
+          {productTitle}
+        </h1>
+
+        <div
+          className="flex justify-between items-center"
+        >
+          <h2 className="product-price">
+            ${productPrice.toFixed(2)}
+          </h2>
+
+          <span
+            className="flex items-center gap-8"
+          >
+            <img
+              className="w-16px"
+              src={ratingIcon}
+              alt="rating icon"
+            />
+            {productRating}
+          </span>
+        </div>
+
+        <span className="text-gray">
+          Stock: {productStock}
+        </span>
+
+      </div>
+
+
+      <div
+        className="bg-background flex flex-col m-16 gap-12 p-24 rounded-md"
+      >
+        <h3>Description</h3>
+
+        <p
+          className="text-gray text-left"
+        >
+          {productDescription}
+        </p>
+      </div>
+
+      <details
+        className="bg-background flex flex-col p-24 m-16 rounded-md"
+      >
+        <summary
+          className="flex justify-between items-center cursor-pointer"
+        >
+          <span className="font-bold">
+            More Details
+          </span>
+
+          <span>
+            <img
+              className="details-icon w-16px"
+              src={arrowDown}
+              alt="toggle-icon"
+            />
+          </span>
+        </summary>
+
+        <ul
+          className="flex flex-col gap-8 mt-8 text-gray"
+        >
+          <li className="flex justify-between">
+            <span>Brand</span>
+            <span>{productBrand}</span>
+          </li>
+
+          <li className="flex justify-between">
+            <span>Weight</span>
+            <span>{productWeight}</span>
+          </li>
+
+          <li className="flex justify-between">
+            <span>Dimensions</span>
+            <span>
+              {productDimensions.width} × {productDimensions.height} ×
+              {productDimensions.depth}
+            </span>
+          </li>
+
+          <li className="flex justify-between">
+            <span>Warranty</span>
+            <span>{productWarranty}</span>
+          </li>
+        </ul>
+
+      </details>
+
+      <button
+        className="primary-button flex justify-center items-center gap-8 m-16 p-16 rounded-sm"
+        onClick={() => addToCart(product)}
+      >
+        <span>Add to cart</span>
         <img
-          className="w-16px"
-          src={ratingIcon}
-          alt="rating icon"
+          className="w-24px"
+          src={addToCartIcon}
+          alt="cart-icon"
         />
-        {productRating}
-      </span>
-    </div>
+      </button>
 
-    <span className="text-gray">
-      Stock: {productStock}
-    </span>
-
-  </div>
-
-
-  <div
-    className="bg-background flex flex-col m-16 gap-12 p-24 rounded-md"
-  >
-    <h3>Description</h3>
-
-    <p
-      className="text-gray text-left"
-    >
-      {productDescription}
-    </p>
-  </div>
-  
-  <details
-  className="bg-background flex flex-col p-24 m-16 rounded-md"
->
-  <summary
-    className="flex justify-between items-center cursor-pointer"
-  >
-    <span className="font-bold">
-      More Details
-    </span>
-
-    <span>
-      <img 
-        className="details-icon w-16px"
-        src={arrowDown}
-        alt="toggle-icon"
-      />
-    </span>
-  </summary>
-
-  <ul
-    className="flex flex-col gap-8 mt-8 text-gray"
-  >
-    <li className="flex justify-between">
-      <span>Brand</span>
-      <span>{productBrand}</span>
-    </li>
-
-    <li className="flex justify-between">
-      <span>Weight</span>
-      <span>{productWeight}</span>
-    </li>
-
-    <li className="flex justify-between">
-      <span>Dimensions</span>
-      <span>
-        {productDimensions.width} × {productDimensions.height} ×
-        {productDimensions.depth}
-      </span>
-    </li>
-
-    <li className="flex justify-between">
-      <span>Warranty</span>
-      <span>{productWarranty}</span>
-    </li>
-  </ul>
-
-</details>
-
-<button
-  className="primary-button flex justify-center items-center gap-8 m-16 p-16 rounded-sm"
-  onClick={() => addToCart(product)}
->
-  <span>Add to cart</span>
-  <img
-    className="w-24px"
-    src={addToCartIcon}
-    alt="cart-icon"
-  />
-</button>
-
-</section>
+    </section>
   )
 }
 
