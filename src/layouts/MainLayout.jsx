bf1412470fe57ebee12d284212801b3a9d110faf
Cristhian B <cristhianblaffita@gmail.com>
2026-03-30 11:42:30 -0400

    refact: change api products limit to manage more products

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,57 +1,51 @@
-import "./MainLayout.css"
-import { Outlet } from "react-router-dom"
-import { CartProvider } from "@/contexts/cart/CartContext"
+import "./MainLayout.css";
+import { Outlet } from "react-router-dom";
+import { CartProvider } from "@/contexts/cart/CartContext";
 
-import useToggleMenu from "@/layouts/menu/hooks/useToggleMenu"
-import useSearchSection from "@/layouts/search_section/hooks/useSearchSection"
-import { useProducts } from "@/hooks/useProducts"
+import useToggleMenu from "@/layouts/menu/hooks/useToggleMenu";
+import useSearchSection from "@/layouts/search_section/hooks/useSearchSection";
+import { useProducts } from "@/hooks/useProducts";
 
-import NavBar from "@/layouts/navbar/components/NavBar"
-import Menu from "@/layouts/menu/components/Menu"
-import SearchSection from "@/layouts/search_section/components/SearchSection"
-
+import NavBar from "@/layouts/navbar/components/NavBar";
+import Menu from "@/layouts/menu/components/Menu";
+import SearchSection from "@/layouts/search_section/components/SearchSection";
 
 const MainLayout = () => {
-  const { products, isLoading: productsLoading  } = useProducts("https://dummyjson.com/products?limit=149")
+  const { products, isLoading: productsLoading } = useProducts(
+    "https://dummyjson.com/products?limit=0",
+  );
 
-  const { isOpen, toggleMenu } = useToggleMenu()
-  
+  const { isOpen, toggleMenu } = useToggleMenu();
+
   const {
     isOpen: searchOpen,
     toggleSearchSection,
     query,
     handleChange,
-    handleClear
-  } = useSearchSection()
-  
+    handleClear,
+  } = useSearchSection();
+
   return (
     <>
       <CartProvider>
-
         <header>
           <NavBar
             toggleMenu={toggleMenu}
             toggleSearchSection={toggleSearchSection}
           />
-          <Menu
-            toggleMenu={toggleMenu}
-            isOpen={isOpen}
-            products={products}
-          />
+          <Menu toggleMenu={toggleMenu} isOpen={isOpen} products={products} />
           <SearchSection
             isOpen={searchOpen}
             handleBack={toggleSearchSection}
             query={query}
             handleChange={handleChange}
-            handleClear={handleClear} 
+            handleClear={handleClear}
             products={products}
           />
         </header>
 
-        <main
-          className="outlet"
-        >
-          <Outlet context={{ query, products, productsLoading }}/>
+        <main className="outlet">
+          <Outlet context={{ query, products, productsLoading }} />
         </main>
 
         <footer>
@@ -59,7 +53,7 @@ const MainLayout = () => {
         </footer>
       </CartProvider>
     </>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
