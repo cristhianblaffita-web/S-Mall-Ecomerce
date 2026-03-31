0f791b30c8a9868b72f8660c5e8d9d83a9ee9731
Cristhian B <cristhianblaffita@gmail.com>
2026-03-31 01:17:59 -0400

    refact: make cart responsive to desltop environment

--- src/features/cart/components/CartItem.jsx
+++ src/features/cart/components/CartItem.jsx
@@ -1,79 +1,52 @@
-import { Link } from "react-router-dom"
-import { useCart } from "@/contexts/cart/useCart"
-import "./CartItem.css"
+import { Link } from "react-router-dom";
+import { useCart } from "@/contexts/cart/useCart";
+import "./CartItem.css";
 
-const CartItem = (
-  {
-    itemId = null,
-    itemImage = null,
-    itemTitle = null,
-    price = 0,
-    qtty = 0,
-    subtotal = 0,
-    handleRemove = null
-  }
-) => {
-
-  const {
-    decreaseItemQtty,
-    increaseItemQtty,
-    removeFromCart
-  } = useCart()
-
-
+const CartItem = ({
+  itemId = null,
+  itemImage = null,
+  itemTitle = null,
+  price = 0,
+  qtty = 0,
+  subtotal = 0,
+  handleRemove = null,
+}) => {
+  const { decreaseItemQtty, increaseItemQtty, removeFromCart } = useCart();
 
   return (
-    <div
-      className="cart-item w-full p-16 bg-background"
-    >
-      <Link
-        to={`/products/${itemId}`}
-        className="item-img-link"
-      >
-        <img
-          className="item-img w-full h-auto"
-          style={{
-            width: "40%"
-          }}
-          src={itemImage}
-        />
+    <div className="cart-item w-full p-16 bg-background">
+      <Link to={`/products/${itemId}`} className="item-img-link">
+        <img className="item-img " src={itemImage} />
       </Link>
-      <Link
-        to={`/products/${itemId}`}
-        className="item-title"
-      >
+      <Link to={`/products/${itemId}`} className="item-title">
         <p>{itemTitle}</p>
       </Link>
 
-      <span
-        className="item-subtotal"
-      >Subtotal: ${subtotal.toFixed(2)}</span>
+      <span className="item-subtotal">Subtotal: ${subtotal.toFixed(2)}</span>
 
-      <span
-        className="item-price"
-      >Price: ${price.toFixed(2)}</span>
+      <span className="item-price">Price: ${price.toFixed(2)}</span>
 
-      <div
-        className="item-qtty w-full flex p-8"
-      >
+      <div className="item-qtty w-full flex p-8">
         <button
           className="primary-button"
           onClick={() => decreaseItemQtty(itemId)}
-        >-</button>
+        >
+          -
+        </button>
         <span>{qtty}</span>
         <button
           className="primary-button"
           onClick={() => increaseItemQtty(itemId)}
-        >+</button>
+        >
+          +
+        </button>
       </div>
 
-      <button
-        className="item-remove"
-        onClick={() => removeFromCart(itemId)}
-      >Remove</button>
-
+      <button className="item-remove" onClick={() => removeFromCart(itemId)}>
+        Remove
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,51 +1,56 @@
-import { useCart } from "@/contexts/cart/useCart"
-import CartItem from "@/features/cart/components/CartItem.jsx"
+import "@/features/cart/Cart.css";
+import { useCart } from "@/contexts/cart/useCart";
+import CartItem from "@/features/cart/components/CartItem.jsx";
 
 const Cart = () => {
-  const { cartItems, calculateCartTotal } = useCart()
-  
+  const { cartItems, calculateCartTotal } = useCart();
+
   return (
-    <main
-      className="w-full h-full relative flex flex-col gap-8"
-    >
-      <div
-        className="w-full bg-surface p-32 shadow-sm mt-8"
-      >
+    <main className="cart w-full h-full relative flex flex-col gap-8">
+      <div className="cart-header w-full bg-surface p-32 shadow-sm mt-8">
         <h1>Cart</h1>
       </div>
-      
+
       <section
-        className="w-full h-full flex flex-col gap-16 padding-tb-16 bg-surface shadow-sm mb-120 text-gray
+        className="cart-list w-full  h-full flex flex-col gap-16 padding-tb-16 bg-surface shadow-sm mb-120 text-gray
         text-center"
       >
-        {cartItems.length > 0 ? cartItems.map((item) => (
-          <CartItem 
-            key={item.id}
-            itemId={item.id}
-            itemImage={item.image}
-            itemTitle={item.title}
-            price={item.price}
-            qtty={item.qtty}
-            subtotal={item.subtotal}
-          />
-        )) : <p className="p-32">Your S-Mall cart is empty</p>}
+        {cartItems.length > 0 ? (
+          cartItems.map((item) => (
+            <CartItem
+              key={item.id}
+              itemId={item.id}
+              itemImage={item.image}
+              itemTitle={item.title}
+              price={item.price}
+              qtty={item.qtty}
+              subtotal={item.subtotal}
+            />
+          ))
+        ) : (
+          <p className="p-32">Your S-Mall cart is empty</p>
+        )}
       </section>
-      
-      { cartItems.length > 0 ? (<div
-        className="fixed bottom-left flex flex-col w-full bg-surface p-32 shadow-lg mt-8
-        gap-8"
-      >
-        <div
-          className="w-full flex justify-end"
-        >
-          <span><strong>Amount: </strong>${calculateCartTotal().toFixed(2)}</span>
-        </div>
-        <button
-          className="primary-button w-full p-16 rounded-md text-normal text-lg"
-        >Checkout</button> 
-      </div>) : false}
-    </main>
-  )
-}
 
-export default Cart
+      {cartItems.length > 0 ? (
+        <div
+          className="checkout-section fixed bottom-left flex flex-col w-full bg-surface p-32 shadow-lg mt-8
+        gap-8"
+        >
+          <div className="w-full flex justify-end">
+            <span>
+              <strong>Amount: </strong>${calculateCartTotal().toFixed(2)}
+            </span>
+          </div>
+          <button className="primary-button w-full p-16 rounded-md text-normal text-lg">
+            Checkout
+          </button>
+        </div>
+      ) : (
+        false
+      )}
+    </main>
+  );
+};
+
+export default Cart;
