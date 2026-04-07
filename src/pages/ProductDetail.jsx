0e2f0bad3370269cc4e6a49c4bfa4f8695cb6cd2
Cristhian B <cristhianblaffita@gmail.com>
2026-04-07 16:58:18 -0400

    refact: improve data state managment in ProductDetails

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,18 +1,32 @@
 import { useParams } from "react-router-dom";
 import { useProducts } from "@/hooks/useProducts";
+import DataStateHandler from "@/features/ui_states/DataStateHandler";
+import ErrorState from "@/features/ui_states/error_state/ErrorState";
+import EmptyState from "@/features/ui_states/empty_state/EmptyState";
 import ProductDetails from "@/features/details/components/ProductDetails";
 import DetailsSkeleton from "@/features/skeletons/details_skeleton/components/DetailsSkeleton";
 
 const ProductDetail = () => {
   const { id } = useParams();
 
-  const { products, isLoading } = useProducts(
+  const { products, isLoading, error } = useProducts(
     `https://dummyjson.com/products/${id}`,
   );
 
   return (
     <main>
-      {isLoading ? <DetailsSkeleton /> : <ProductDetails product={products} />}
+      {
+        <DataStateHandler
+          isLoading={isLoading}
+          loadingComponent={<DetailsSkeleton />}
+          isEmpty={!products.length}
+          emptyComponent={<EmptyState />}
+          error={error}
+          errorComponent={<ErrorState />}
+        >
+          <ProductDetails product={products} />
+        </DataStateHandler>
+      }
     </main>
   );
 };
