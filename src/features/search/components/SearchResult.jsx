60225c264f33c1137c62955ff327369cc9a1d7ff
cristhianblaffita-web <cristhianblaffita@gmail.com>
2026-04-18 04:00:10 -0400

    refact(SearchResult): add the product rating to the search results

--- src/features/search/components/SearchResult.jsx
+++ src/features/search/components/SearchResult.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import { setDiscount } from "@/utils/setDiscount"
+import RatingIcon from "@/assets/icons/ui/star.png"
 import "./SearchResult.css"
 
 const SearchResult = ({ product }) => {
@@ -7,13 +8,13 @@ const SearchResult = ({ product }) => {
 
   return (
     <Link
-        className="search-result bg-surface p-12 rounded-md decoration-none"
-        to={`/products/${product.id}`}
+      className="search-result bg-surface p-12 rounded-md decoration-none"
+      to={`/products/${product.id}`}
     >
       <img
         src={product.thumbnail}
         alt={product.title}
-        className="w-full object-cover rounded-md mb-8"
+        className="item-image w-full object-cover rounded-md mb-8"
         onLoad={(e) => e.currentTarget.classList.add("loaded")}
       />
 
@@ -25,12 +26,19 @@ const SearchResult = ({ product }) => {
         {product.category}
       </p>
 
-      <div className="flex gap-8">
+      <div className="flex items-center gap-8 mt-4">
+        <img className="rating-icon" src={RatingIcon} />
+        <span className="text-xs text-gray">
+          {product.rating.toFixed(1)}
+        </span>
+      </div>
+
+      <div className="flex gap-8 items-center">
         <p className="text-sm font-medium mt-4 text-normal">
-            ${productDiscount.toFixed(2)}
+          ${productDiscount.toFixed(2)}
         </p>
         <p className="text-xs text-gray line-tr">
-            ${product.price}
+          ${product.price}
         </p>
       </div>
     </Link>
